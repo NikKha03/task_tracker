8806049882c825b0f327a84394ca159c3620f3e7
NikKha03 <kolyakhalimendik@gmail.com>
2025-03-30 00:27:17 +0300

    add

--- frontend_new/src/components/Navbar.jsx
+++ frontend_new/src/components/Navbar.jsx
@@ -1,56 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 import { MDBContainer, MDBNavbar, MDBNavbarBrand, MDBInputGroup, MDBInput, MDBIcon, MDBBtn, MDBNavbarLink } from 'mdb-react-ui-kit';
 
-export default function Navbar({ pageType, user, project }) {
+export default function Navbar({ pageType, user }) {
 	const navigate = useNavigate();
 
+	const names = [
+		{ name: 'Нужно сделать', icon: <MDBIcon far size='lg' icon='folder' /> },
+		{ name: 'В работе', icon: <MDBIcon far size='lg' icon='folder' /> },
+		{ name: 'Просроченные', icon: <MDBIcon far size='lg' icon='folder' /> },
+		{ name: 'Завершенные', icon: <MDBIcon far size='lg' icon='folder' /> },
+	];
+
+	const [idClicked, setIdClicked] = useState(0);
+
+	const taskStatus = (id, icon, name) => {
+		return (
+			<div className={`task-status ${idClicked === id ? 'active' : ''}`} key={id} onClick={() => setIdClicked(id)}>
+				<div style={{ display: 'flex' }}>
+					<div className='icon'>{icon}</div>
+					<h2>{name}</h2>
+				</div>
+			</div>
+		);
+	};
 	const projBlokForList = () => {
 		return (
 			<>
 				<div className='top'>{/* <h4>t</h4> */}</div>
-				<div className='bottom'>
-					<div className='task-status'>
-						<div style={{ display: 'flex' }}>
-							<div className='icon'>
-								<MDBIcon far size='lg' icon='folder' />
-							</div>
-							<h2>Нужно сделать</h2>
-						</div>
-					</div>
-
-					<div className='task-status'>
-						<div style={{ display: 'flex' }}>
-							<div className='icon'>
-								<MDBIcon far size='lg' icon='folder' />
-							</div>
-							<h2>В работе</h2>
-						</div>
-					</div>
-
-					<div className='task-status'>
-						<div style={{ display: 'flex' }}>
-							<div className='icon'>
-								<MDBIcon far size='lg' icon='folder' />
-							</div>
-							<h2>Просроченные</h2>
-						</div>
-					</div>
-
-					<div className='task-status'>
-						<div style={{ display: 'flex' }}>
-							<div className='icon'>
-								<MDBIcon far size='lg' icon='folder' />
-							</div>
-							<h2>Завершенные</h2>
-						</div>
-					</div>
-				</div>
+				<div className='bottom'>{names.map((obj, i) => taskStatus(i, obj.icon, obj.name))}</div>
 			</>
 		);
 	};
+
 	const projBlokForBoard = () => {
 		return <h1>Board</h1>;
 	};
