d8591c1aab2ede8044e424d0a940398223b1fefd
NikKha03 <kolyakhalimendik@gmail.com>
2026-08-01 03:07:58 +0300

    Обновил сборку

--- api_gateway/src/main/java/com/NikKha03/EurekaServer/config/GatewayConfig.java
+++ api_gateway/src/main/java/com/NikKha03/EurekaServer/config/GatewayConfig.java
@@ -1,5 +1,6 @@
 package com.NikKha03.EurekaServer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,25 +9,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+    @Value("${app-env.gateway-secret}")
+    private String gatewaySecret;
+
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("task_service", r -> r.path("/api/task_service/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://TASKSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 )
                 .route("task_service", r -> r.path("/api/swagger-ui/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://TASKSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 )
                 .route("user_service", r -> r.path("/api/user_service/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://USERSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 ) 
                 .route("user_service", r -> r.path("/api/login/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://USERSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 )
                 .route("user_service", r -> r.path("/api/oauth2/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://USERSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 )
                 .route("notification_service", r -> r.path("/api/notification_service/**")
+                        .filters(f -> f.addRequestHeader("X-Gateway-Secret", gatewaySecret))
                         .uri("lb://NOTIFICATIONSERVICE")  // lb:// для балансировки нагрузки через Eureka
                 )
                 .build();
